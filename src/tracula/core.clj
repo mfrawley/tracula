0faa757269db1e19d1e062fafd60226057d40393
Mark Frawley <mark@jimdo.com>
2013-11-01 15:21:08 +0100

    added get tickets fields call

--- src/tracula/core.clj
+++ src/tracula/core.clj
@@ -50,26 +50,29 @@
 (defn format-ts-for-json [datetime-str]
 	{"__jsonclass__" ["datetime" datetime-str] })
 
+(defn jsonify [value] (json/write-str value))
+
 (defn list-methods []
 	(api-req "system.listMethods" []))
 
 (defn get-method-help [method]
 	(api-req "system.methodHelp" [method]))
 
-(defn get-ticket-actions [ticketno]
-	(api-req "ticket.getActions" [ticketno]))
-
 (defn hashify-changelog-entry [changelog-arr]
 	(let [[dtime, author, field, oldvalue, newvalue, permanent ] changelog-arr]
 	{:time dtime :author author :field field :oldvalue oldvalue :newvalue newvalue :permanent permanent}))
 
-(defn get-ticket-changelog [ticketno]
-	 (map hashify-changelog-entry (api-req "ticket.changeLog" [ticketno])))
-
+;;ticket methods
 (defn get-ticket [ticketno]
 	(let [res (api-req "ticket.get" [ticketno])]
 	 	(if (res 0) {:id (res 0) :time_created (res 1) :time_changed (res 2) :attributes (res 3)}
 			{:error "Ticket not found."})))
+
+(defn get-ticket-actions [ticketno]
+	(api-req "ticket.getActions" [ticketno]))
+
+(defn get-ticket-changelog [ticketno]
+	 (map hashify-changelog-entry (api-req "ticket.changeLog" [ticketno])))
 
 (defn update-ticket [ticketno commentstr action notify]
 	(let [notify false action "done" attrs {:_ts (get-current-timestamp-str) :action "done"}]
@@ -82,10 +85,12 @@
 (defn delete-ticket [ticketno]
 	(api-req "ticket.delete" [ticketno]))
 
+(defn get-ticket-fields []
+	(api-req "ticket.getTicketFields" []))
+
 (defn get-recent []
 	(api-req "ticket.getRecentChanges" [(format-ts-for-json (get-current-timestamp-str))]))
 
-(defn jsonify [value] (json/write-str value))
 
 ; too new
 ; (defn query-tickets [query]
@@ -109,11 +114,13 @@
   (compcore/GET "/help/:method" [method] (get-method-help method))
   ;;;;;;;;;;;;;;
   ;ticket routes
+  (compcore/GET "/tickets/fields" [] (jsonify (get-ticket-fields)))
   (compcore/GET "/tickets/:id" [id] (jsonify (get-ticket (read-string id))))
   (compcore/GET "/tickets/:id/actions" [id] (jsonify (get-ticket-actions (read-string id))))
 
   (compcore/POST "/tickets" {params :params} (rest-create-ticket params))
-  (compcore/DELETE "/tickets/:id" [id] (jsonify (delete-ticket (read-string id))))
+    (compcore/DELETE "/tickets/:id" [id] (jsonify (delete-ticket (read-string id))))
+
 
   ; (compcore/PUT "/tickets/:id" [id] (jsonify (update-ticket (read-string id) commentstr action notify)))
   )
