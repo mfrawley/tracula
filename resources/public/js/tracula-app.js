c3cb3fcf5d387ffec21e7d5740651367cb5101d7
Mark Frawley <markfrawley@gmail.com>
2013-11-16 09:20:04 +0100

    routing and template rendering works, data not yet rendered

--- resources/public/js/tracula-app.js
+++ resources/public/js/tracula-app.js
@@ -2,49 +2,50 @@ Tracula = Ember.Application.create();
 Tracula.baseUrl = 'http://localhost:8080';
 
 Tracula.Store = DS.Store.extend({
-  revision: 12
+    revision: 12
 });
 
 Tracula.Router.map(function() {
-  this.route("search");
+    // this.route("ticket");
+    // this.resource('tickets');
+    this.resource('ticket', { path: '/ticket/:ticket_id' });
 });
 
 Tracula.ApplicationRoute = Ember.Route.extend({
-  setupController: function(controller) {
-    // `controller` is the instance of ApplicationController
-    controller.set('title', "Hello world!");
-  }
-});
-
-Tracula.SearchRoute = Ember.Route.extend({
-  model: function() {
-    var url = Tracula.baseUrl + '/tickets/60988';
-    return Ember.$.getJSON(url).then(function(data) {
-      console.log(data);
-    });
-  }
+    setupController: function(controller) {
+        // `controller` is the instance of ApplicationController
+        controller.set('title', "Hello world!");
+    }
 });
 
 Tracula.ApplicationController = Ember.Controller.extend({
-	prevQuery : '',
-	searchQuery : '',
-  appName: "Tracula - It may suck but it won't bite",
-	searchQueryDidChange : function(q) {
-		if(this.searchQuery.length < 3) {
-			console.log('same or too short');
-			return;
-		}
+    prevQuery : '',
+    searchQuery : '',
+    appName: "Tracula - It may suck but it won't bite",
+    searchQueryDidChange : function(q) {
+        if(this.searchQuery == '') return;
+        if(this.searchQuery.length < 3) {
+            console.log('same or too short');
+            return;
+        }
 
-		console.log(this.searchQuery);
-		this.prevQuery = this.searchQuery;
-	}.observes('searchQuery').on('init')
+        console.log(this.searchQuery);
+        this.prevQuery = this.searchQuery;
+    }.observes('searchQuery').on('init')
 });
 
-Tracula.SearchController = Ember.Controller.extend({
-	tickets: [{
-		'id' : 2,
-		'title' : 'sdfdsf',
-		'status' : 'open',
-		'assigned_to' : 'mark'
-	}]
+Tracula.TicketRoute = Ember.Route.extend({
+    model: function(params) {
+        var url = Tracula.baseUrl + '/tickets/'+params.ticket_id;
+        return Ember.$.getJSON(url).then(function(data) {
+            // console.log(data);
+            return data;
+        });
+    }
+});
+
+Tracula.TicketController = Ember.Controller.extend({
+    setupController: function(controller, model) {
+        controller.set('model', model);
+    }
 })
