88d1824c1eef7c354299c0ea71e6022b05216607
Mark Frawley <mark@jimdo.com>
2013-11-22 11:35:19 +0100

    first test written

--- test/tracula/test/core.clj
+++ test/tracula/test/core.clj
@@ -2,5 +2,11 @@
   (:use [tracula.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(defn has-keys [expected-keys obj]
+	(is expected-keys (keys obj)))
+
+(testing "API - basic tests"
+	(deftest test-get-ticket
+  	(let [ticket (get-ticket 61450)]
+  		(is (type ticket) clojure.lang.PersistentArrayMap)
+  		(has-keys [:id :time_created :time_changed :attributes] ticket))))
