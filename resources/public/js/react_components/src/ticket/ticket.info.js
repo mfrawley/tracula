ad82c570991f53567364a288cb993632d7396edd
Mark Frawley <markfrawley@gmail.com>
2014-01-26 12:01:23 +0100

    working on rake task

--- resources/public/js/react_components/src/ticket/ticket.info.js
+++ resources/public/js/react_components/src/ticket/ticket.info.js
@@ -13,32 +13,32 @@ var TicketInfo = React.createClass( {
       );
     } else {
       return (
-      div(null, 
-        div( {className:"row"}, 
-          div( {className:"col-md-2"}, "Type:"),
-          div( {className:"col-md-1"}, "Priority:"),
-          div( {className:"col-md-1"}, "Status:"),
-          div( {className:"col-md-2"}, "Owner:"),
-          div( {className:"col-md-2"}, "Reported by:"),
-          div( {className:"col-md-2"}, "Component:")
-        ),
+        div(null, 
+          div( {className:"row"}, 
+            div( {className:"col-md-2"}, "Type:"),
+            div( {className:"col-md-1"}, "Priority:"),
+            div( {className:"col-md-1"}, "Status:"),
+            div( {className:"col-md-2"}, "Owner:"),
+            div( {className:"col-md-2"}, "Reported by:"),
+            div( {className:"col-md-2"}, "Component:")
+          ),
 
-        div( {className:"row"}, 
-          div( {className:"col-md-2"}, p.type),
-          div( {className:"col-md-1"}, p.priority),
-          div( {className:"col-md-1"}, p.status),
-          div( {className:"col-md-2"}, p.owner),
-          div( {className:"col-md-2"}, p.reporter),
-          div( {className:"col-md-2"}, p.component)
-        ),
-        
-        div( {className:"row"}, 
-          d.label( {htmlFor:"description"}, "Description"),
-          d.pre( {className:"col-md-12", id:"description", contentEditable:"true", onInput:p.ticketChanged}, 
-            p.description
+          div( {className:"row"}, 
+            div( {className:"col-md-2"}, p.type),
+            div( {className:"col-md-1"}, p.priority),
+            div( {className:"col-md-1"}, p.status),
+            div( {className:"col-md-2"}, p.owner),
+            div( {className:"col-md-2"}, p.reporter),
+            div( {className:"col-md-2"}, p.component)
+          ),
+          
+          div( {className:"row"}, 
+            d.label( {htmlFor:"description"}, "Description"),
+            d.pre( {className:"col-md-12", id:"description", contentEditable:"true", onInput:p.ticketChanged}, 
+              p.description
+            )
           )
         )
-      )
       );  
     }
     
